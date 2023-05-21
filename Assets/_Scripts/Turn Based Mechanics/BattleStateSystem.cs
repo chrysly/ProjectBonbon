f60c3d19a872033f2137269693bb6d72ebc00596
clee857 <chrysly0816@gmail.com>
2023-05-21 01:20:21 -0500

    UI placeholders

--- Assets/_Scripts/Turn Based Mechanics/BattleStateSystem.cs
+++ Assets/_Scripts/Turn Based Mechanics/BattleStateSystem.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WIN, LOSE }
+
 public class BattleStateSystem : MonoBehaviour
 {
+
+    public BattleState battleState;
+
+    #region Battle Actors
+    [SerializeField] private List<CharacterActor> actorList;
+    [SerializeField] private List<EnemyActor> enemyList;
+    #endregion Battle Actors
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        battleState = BattleState.START;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void InitializeBattle() {
+
     }
 }
