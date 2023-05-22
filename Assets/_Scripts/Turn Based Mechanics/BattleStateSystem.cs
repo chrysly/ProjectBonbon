ca31d4080f7917251fbb0d9560d5df3e431c726a
clee857 <chrysly0816@gmail.com>
2023-05-21 22:53:06 -0500

    redoing battle state

--- Assets/_Scripts/Turn Based Mechanics/BattleStateSystem.cs
+++ Assets/_Scripts/Turn Based Mechanics/BattleStateSystem.cs
@@ -8,16 +8,20 @@ public class BattleStateSystem : MonoBehaviour
 {
 
     public BattleState battleState;
+    public Transform activeActor;
 
     #region Battle Actors
     [SerializeField] private List<CharacterActor> actorList;
     [SerializeField] private List<EnemyActor> enemyList;
+    [SerializeField] private List<Transform> actorSpawnPoints;
+    [SerializeField] private List<Transform> enemySpawnPoints;
     #endregion Battle Actors
 
     // Start is called before the first frame update
     void Start()
     {
         battleState = BattleState.START;
+        InitializeBattle();
     }
 
     // Update is called once per frame
@@ -26,7 +30,14 @@ public class BattleStateSystem : MonoBehaviour
         
     }
 
-    void InitializeBattle() {
+    private IEnumerator InitializeBattle() {
 
+        //TODO: Initialize actors at actor spawn points
+        yield return new WaitForSeconds(3f);
     }
+
+    #region Game States
+
+
+    #endregion Game States
 }
